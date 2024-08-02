0decc01ea23f99bf022f257e13e4ad6dff84e424
mankind4dev <ogunmakin120@gmail.com>
2024-08-02 21:00:46 +0100

    update

---
+++ src/responsive/Responsive.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactResponsive from './react/ReactResponsive'
+
+export default function Responsive() {
+  return (
+    <>
+       <ReactResponsive /> 
+    </>
+  )
+}
---
+++ src/responsive/react/ReactResponsive.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReactResponsive() {
+  return (
+    <div>
+        nhgfdzsdfghj
+    </div>
+  )
+}
